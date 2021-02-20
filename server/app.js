cfdbd927274ca959b8fe90b2b0afe40c795e96d3
ImaneChafi <imane.chafi@mail.mcgill.ca>
2021-02-20 07:54:38 -0500

    Added login app logic
    Also changed from pug to html

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser'); // middleware
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,9 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
