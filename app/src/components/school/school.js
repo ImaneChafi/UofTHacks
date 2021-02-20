cc84fe0cc77fda3803c93f9b00cbab93bfd97050
Kyoji <kyoji.goto@mail.utoronto.ca>
2021-02-20 17:28:15 -0500

    Created chat component

--- app/src/components/school/school.js
+++ app/src/components/school/school.js
@@ -1,42 +1,53 @@
 import React, { useState } from 'react'
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import SettingsIcon from '@material-ui/icons/Settings'
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Navigation from '../navigation/navigation.js'
+import Thread from '../thread/thread.js'
 
-function sideBarComponent() {
+function SideBarComponent(props) {
+  console.log(props)
   return (
-    <>
-      <AccountCircle/> 
       <List>
         {/** Add a list of classes and their threads (as well as personal threads?)
         The buttons should change what shows up in the main component 
         */}
-          {['General', 'Settings', 'Log Out'].map((text, index) => (
+          {props.user.classes.map((text, index) => (
           <ListItem button key={text}>
-              <ListItemText primary={text} />
+              <ListItemText primary={text} onClick={(e) => props.setThread(e.target.innerText)}/>
           </ListItem>
           ))}
       </List>
-    </>
   )
 }
 
-function mainComponent() {
+function MainComponent(props) {
   {/** Create the layout and how to show the thread */}
   return (
-    <p>This a schools ting u no</p>
+    <Thread 
+      thread={props.thread}
+      user={props.user}
+      messages={props.messages}
+    />
   )
 }
 
 
-function School() {
+function School(props) {
+    const [thread, setThread] = useState("")
+    const messages = [{text: "Hey", name: "Kyoji", created_at: "09:30"}, {text: "hi", name: "Paul", created_at: "09:31"}, {text: "what's up guys", name: "Imane", created_at: "09:32"}, {text: "nm hbu", name: "Paul", created_at: "09:31"}]
+
     return (
         <div className="wrapper">
           <Navigation
-            sideBarComponent={sideBarComponent()}
-            mainComponent={mainComponent()}
+            sideBarComponent={<SideBarComponent
+              user={props.user}
+              setThread={setThread}
+            />}
+            mainComponent={thread !== "" ? 
+            <MainComponent
+              user={props.user}
+              thread={thread}
+              messages={messages}
+            /> : <p>Please click on a class to see its thread.</p>}
           />
         </div>
     )
---
+++ app/src/components/thread/thread.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Fab from '@material-ui/core/Fab';
+import SendIcon from '@material-ui/icons/Send';
+
+const useStyles = makeStyles({
+  messageArea: {
+    height: '70vh',
+    overflowY: 'auto'
+  },
+  avatar: {
+      float: 'left',
+      "margin-right": '5px'
+  }
+});
+
+function Chat(props) {
+  const classes = useStyles();
+  console.log(props)
+
+  return (
+      <div>
+        <Grid container>
+            <Grid item xs={12} >
+                <Typography variant="h5" className="header-message">{props.thread}</Typography>
+            </Grid>
+        </Grid>
+            <Grid item xs={12} >
+                <List className={classes.messageArea}>
+                    {props.messages.map((message, index) => (
+                        <ListItem key={index}>
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <Avatar alt={message.name} src="/static/images/avatar/1.jpg" className={classes.avatar}/>
+                                <ListItemText align="left" primary={message.text}></ListItemText>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <ListItemText align="left" secondary={"at " + message.created_at + " from: " + message.name}></ListItemText>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <Grid container style={{padding: '20px'}}>
+                    <Grid item xs={11}>
+                        <TextField id="outlined-basic-text" label="Type your message here" fullWidth />
+                    </Grid>
+                    <Grid xs={1} align="right">
+                        <Fab color="primary" aria-label="add"><SendIcon /></Fab>
+                    </Grid>
+                </Grid>
+            </Grid>
+      </div>
+  );
+}
+
+export default Chat;
