beaff2ab923532c5714b3fe64b015c2fee512d92
Kyoji <kyoji.goto@mail.utoronto.ca>
2021-02-20 15:05:24 -0500

    Added routes to all pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import School from './components/school/school.js'
+import Games from './components/games/games.js'
+import Feed from './components/feed/feed.js'
 import Profile from './components/profile/profile.js'
 import Login from './components/login/login.js'
 import './App.css';
 
 function App() {
-  const [login, setLogin] = useState(false)
+  const [login, setLogin] = useState(true)
   if (!login) {
     return (
       <div className="App">
@@ -17,7 +20,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' render={() => <Profile/>} />
+          <Route exact path='/' render={() => <School/>} />
+          <Route exact path='/feed' render={() => <Feed/>} />
+          <Route exact path='/games' render={() => <Games/>} />
+          <Route exact path='/profile' render={() => <Profile/>} />
         </Switch>
       </BrowserRouter>
     </div>
