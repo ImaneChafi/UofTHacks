a6c2c7f2bec2a4fb7ee7ac6410b709d72d0cf3e2
ImaneChafi <imane.chafi@mail.mcgill.ca>
2021-02-20 09:42:35 -0500

    Added user register and login

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const 
+      mongoose              =  require("mongoose"),
+      passport              =  require("passport"),
+      LocalStrategy         =  require("passport-local"),
+      passportLocalMongoose =  require("passport-local-mongoose"),
+      User                  =  require("./models/user");
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser'); // middleware
@@ -22,13 +29,80 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
+//Mongodb setup
 
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/views/login.html');
+require('dotenv').config()
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Imane:hellogovnor@cluster0-cxkrq.mongodb.net/test?retryWrites=true&w=majority');
+const MongoClient = require('mongodb').MongoClient
+var db
+
+MongoClient.connect('mongodb+srv://Imane:hellogovnor@cluster0-cxkrq.mongodb.net/test?retryWrites=true&w=majority', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('test') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set("view engine","ejs");
+
+app.use(require("express-session")({
+  secret:"Any normal Word",       //decode or encode session
+  resave: false,          
+  saveUninitialized:false    
+}));
+passport.serializeUser(User.serializeUser());       //session encoding
+passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded(
+    { extended:true }
+))
+app.use(passport.initialize());
+app.use(passport.session());
+//=======================
+//      R O U T E S
+//=======================
+app.get("/", (req,res) =>{
+  res.render("home");
+})
+app.get("/userprofile",isLoggedIn ,(req,res) =>{
+  res.render("userprofile");
+})
+//Auth Routes
+app.get("/login",(req,res)=>{
+  res.render("login");
 });
+app.post("/login",passport.authenticate("local",{
+  successRedirect:"/userprofile",
+  failureRedirect:"/login"
+}),function (req, res){
+});
+app.get("/register",(req,res)=>{
+  res.render("register");
+});
+app.post("/register",(req,res)=>{
+  
+  User.register(new User({username: req.body.username,phone:req.body.phone,telephone: req.body.telephone}),req.body.password,function(err,user){
+      if(err){
+          console.log(err);
+          res.render("register");
+      }
+  passport.authenticate("local")(req,res,function(){
+      res.redirect("/login");
+  })    
+  })
+})
+app.get("/logout",(req,res)=>{
+  req.logout();
+  res.redirect("/");
+});
+function isLoggedIn(req,res,next) {
+  if(req.isAuthenticated()){
+      return next();
+  }
+  res.redirect("/login");
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,5 +119,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+app.set('view engine', 'ejs')
 
-module.exports = app;
+  const port = process.env.PORT || 3030;
+app.listen(port);
